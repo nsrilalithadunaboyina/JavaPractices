ebed40e030f787cb502a04abd6845c913bf4ae0f
Naga Sri  Lalitha Dunaboyina <nsrilalithadunaboyina@gmail.com>
2025-02-15 18:16:25 +0530

    This code is About My self

---
+++ AboutMyself.java
@@ -0,0 +1,7 @@
+public class AboutMyself {
+    public static void main(String[] args) {
+        System.out.println("A Small Program About my self");
+        System.out.println("Hi EveryOne My Name is Lalitha Iam a Btech graduate " + 2024
+                + "I'm Looking for the positions which meet my skillsets ");
+    }
+}
